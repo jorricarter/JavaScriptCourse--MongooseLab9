6641ce21e070d5441ad90ad28702d1c7ea9f60a8
Jorri <Carter>
2017-11-08 17:07:21 -0600

    can't commit to heroku.  Tried to fix it by deleting .git. it got worse. It works on local before I added code. This version does NOT have the failed code. This version DOES work locally.

---
+++ models/task.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Define your schema: what fields will one task document have
+var taskSchema = new Schema( {
+  text: String,
+  completed: Boolean,
+  dateCreated: Date,
+  dateCompleted: Date,
+});
+
+// Compile taskSchema into Mongoose model object
+var Task = mongoose.model('Task', taskSchema);
+
+// And export the Task so our other code can use it
+module.exports = Task;
+
